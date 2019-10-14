cfe8358169092a51756e17f838e6dde735eb282a
seanaleid <seanaleid@gmail.com>
2019-10-14 18:42:21 -0400

    cannot be closed or opened if it is locked

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,7 +1,7 @@
 // Test away
 
 import React from "react";
-import { render, fireEvents, fireEvent } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import Display from "../display/Display";
 import Controls from "../controls/Controls";
@@ -27,12 +27,32 @@ test("defaults to `unlocked` and `open`", () => {
     expect(open).toBeDefined();
 });
 
-test("cannot be closed or opened if it is locked", () => {
+test("cannot be closed or opened if it is locked - locked: false; closed: false", () => {
     const gateMock = jest.fn();
     
     const { getByText } = render(<Controls locked={ false } closed={ false } />)
 
     fireEvent.click(getByText(/close gate/i));
     expect(gateMock).toHaveBeenCalledTimes(0);
+});
+
+test("cannot be closed or opened if it is locked - locked: false; closed: true", () => {
+    const gateMock = jest.fn();
     
+    const { getByText } = render(<Controls toggleClosed={gateMock} locked={ false } closed={ true } />)
+
+    fireEvent.click(getByText(/open gate/i));
+    expect(gateMock).toHaveBeenCalledTimes(1);
+    expect(gateMock).toBeTruthy();
+});
+
+test("cannot be closed or opened if it is locked - locked: true; closed: true", () => {
+    
+    const lockMock = jest.fn();
+    
+    const { getByText } = render(<Controls toggleLocked={lockMock} locked={ true } closed={ true } />)
+
+    fireEvent.click(getByText(/unlock gate/i));
+    expect(lockMock).toHaveBeenCalledTimes(1);
+    expect(lockMock).toBeTruthy();
 });
