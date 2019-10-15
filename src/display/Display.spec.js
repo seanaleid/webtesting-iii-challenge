530c57c9eb0cf646a7021a5f5c3f136caa29cd0c
seanaleid <seanaleid@gmail.com>
2019-10-15 11:36:16 -0400

    MVP...display component tests pass

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -20,5 +20,54 @@ test("displays if gate is open/closed and if it is locked/unlocked", () => {
 
 });
 
+test("displays 'Closed' if the `closed` prop is `true` and 'Open' if otherwise", () => {
+    let toggle = true;
 
+    const toggler = () => {
+        toggle = false;
+    };
 
+    const { getByText, findByText } = render (<Display closed={toggle} />);
+
+    const closed = getByText(/closed/i);
+
+    expect(closed).toBeDefined();
+
+    toggler();
+
+    const open = findByText(/open/i);
+    expect(open).toBeDefined();
+});
+
+test("displays 'Locked' if the `locked` prop is `true` and 'Unlocked' if otherwise", () => {
+    let toggle = false;
+
+    const toggler = () => {
+        toggle = true;
+    };
+
+    const { getByText, findByText } = render (<Display locked={toggle} />);
+
+    const unlocked = getByText(/unlocked/i);
+
+    expect(unlocked).toBeDefined();
+
+    toggler();
+
+    const locked = findByText(/locked/i);
+    expect(locked).toBeDefined();
+});
+
+test("when `locked` or `closed` use the `red-led` class", () => {
+    const { container } = render(<Display locked={true} closed={true} />);
+    const redLeds = container.querySelectorAll('.red-led');
+
+    expect(redLeds.length).toBe(2);
+});
+
+test("when `unlocked` or `open` use the `green-led` class", () => {
+    const { container } = render(<Display locked={false} closed={false} />);
+    const greenLeds = container.querySelectorAll('.green-led');
+
+    expect(greenLeds.length).toBe(2);
+});
